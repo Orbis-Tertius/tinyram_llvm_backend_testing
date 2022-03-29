4abd0921f2bb43bebe8c296040235bf430c02ebb
Marcin Bugaj <marcin@mlabs.city>
2022-03-29 16:00:46 +0200

    Remove ifdefs

--- lib/builtins.c
+++ lib/builtins.c
@@ -16,7 +16,6 @@ typedef uint32_t fixuint_t;
 #define COMPUTE_UDIV(a, b) ((su_int)(a) / (su_int)(b))
 
 fixint_t __divsi3(fixint_t a, fixint_t b) {
-#if 1
   const int N = (int)(sizeof(fixint_t) * CHAR_BIT) - 1;
   fixint_t s_a = a >> N;                            // s_a = a < 0 ? -1 : 0
   fixint_t s_b = b >> N;                            // s_b = b < 0 ? -1 : 0
@@ -24,9 +23,6 @@ fixint_t __divsi3(fixint_t a, fixint_t b) {
   fixuint_t b_u = (fixuint_t)(b ^ s_b) + (-s_b);    // negate if s_b == -1
   s_a ^= s_b;                                       // sign of quotient
   return (COMPUTE_UDIV(a_u, b_u) ^ s_a) + (-s_a);   // negate if s_a == -1
-#else
-  return 0;
-#endif
 }
 
 si_int __modsi3(si_int a, si_int b) {

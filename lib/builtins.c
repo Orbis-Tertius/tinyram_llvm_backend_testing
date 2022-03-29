50bdfeaf3f2afa41e187d1128623e0c09bcf7ffd
Marcin Bugaj <marcin@mlabs.city>
2022-03-29 16:00:06 +0200

    Add license and reference to https://github.com/llvm/llvm-project/tree/main/compiler-rt

--- lib/builtins.c
+++ lib/builtins.c
@@ -1,3 +1,9 @@
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// This implementation was taken from https://github.com/llvm/llvm-project/tree/main/compiler-rt
+
 #include <stdint.h>
 
 typedef int32_t si_int;
